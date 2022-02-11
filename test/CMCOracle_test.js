ddca98761ed359a7e1f7c32e7c88293b0b441e1d
jochenruland <jochenhgruland@gmail.com>
2022-02-11 09:48:26 +0100

    ADD web3 and provider ganache-cli to testfile

--- test/CMCOracle_test.js
+++ test/CMCOracle_test.js
@@ -1,13 +1,26 @@
-const CMCOracletest = artifacts.require("CMCOracletest");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("CMCOracletest", function (/* accounts */) {
-  it("should assert true", async function () {
-    await CMCOracletest.deployed();
-    return assert.isTrue(true);
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledContractJson = require('../build/contracts/RequestOracle.json');
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  const deploymentKey = Object.keys(compiledContractJson.networks)[0];
+
+  contractInstance = await new web3.eth.Contract(compiledContractJson.abi, compiledContractJson.networks[deploymentKey].address);
+});
+
+
+describe('Testing Oracles', () => {
+  it('Contract deployed to testnet', () => {
+    console.log(contractInstance.options.address);
+    console.log(accounts);
+
+    assert.ok(contractInstance.options.address);
   });
+
 });
