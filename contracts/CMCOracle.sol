47006fe05642b338c26bc8ea5f48b52840bb6cfc
jochenruland <jochenhgruland@gmail.com>
2022-02-13 09:08:44 +0100

    ADD Oracle functions to initiate external datafeed

--- contracts/CMCOracle.sol
+++ contracts/CMCOracle.sol
@@ -1,7 +1,31 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.6.0 <0.9.0;
 
-contract Oracle {
-  constructor() public {
-  }
+contract CMCOracle {
+    address public owner;
+
+    uint public ethMarketCap;
+
+    event CallbackGetEthCap();
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function initCMCDataUpdate() public {
+        emit CallbackGetEthCap();
+    }
+
+    function feedCMCData(uint cap) public onlyOwner {
+      ethMarketCap = cap;
+    }
+
+    function getCMCData() public view returns(uint) {
+      return ethMarketCap;
+    }
+
+    modifier onlyOwner() {
+      require(msg.sender == owner, "Only owner can execute this function");
+      _;
+    }
 }
