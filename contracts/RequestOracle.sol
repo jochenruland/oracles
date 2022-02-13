64b820401e780176e71c2dddff4d15a8fc8a5931
jochenruland <jochenhgruland@gmail.com>
2022-02-13 09:09:52 +0100

    ADD function to request CMCOracle contract

--- contracts/RequestOracle.sol
+++ contracts/RequestOracle.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./CMCOracle.sol";
+
 contract RequestOracle {
-  constructor() public {
+  CMCOracle public cmcO;
+  uint public ethMC;
+
+  function requestCMCOracle() external {
+    ethMC = cmcO.getCMCData();
   }
 }
